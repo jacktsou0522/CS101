a29198cc8e1382901c094ce8b546f64320cd0dec
jacktsou0522 <133513463+jacktsou0522@users.noreply.github.com>
2024-12-12 15:14:23 +0800

    Create main3.c

---
+++ EX4/main3.c
@@ -0,0 +1,10 @@
+int main()
+{
+    for (int y = 1; y <= 9;y++) {
+        for(int x = 1; x <= 9;x++) {
+        printf("%d*%d=%d\t", y,x,y*x);
+        }
+        printf("\n");
+    }
+    return 0;
+}
